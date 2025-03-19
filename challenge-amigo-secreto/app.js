1ae0db5486ac11c827b79d2ac1b0b5174c918e05
rsbm.rm <rm_corporate@hotmail.com>
2025-03-18 22:31:16 -0300

    Atualizar o app.js
    ......

--- challenge-amigo-secreto/app.js
+++ challenge-amigo-secreto/app.js
@@ -132,7 +132,7 @@ function reiniciarSorteio() {
 }
 
 function embaralharArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
+    for (let i = array.length - 1; i > 0, i--) {
         let j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
